c499d362bbce83599ef3a67461ac7d2925ac0a6a
netanel <netamal3134@gmail.com>
2025-03-13 10:59:47 +0200

    הסרת רכיב צור קשר מדף הבית

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import { Box } from '@mui/material';
 import Hero from '@/components/home/Hero';
 import Services from '@/components/home/Services';
-import Contact from '@/components/home/Contact';
 
 export default function Home() {
   return (
     <Box>
       <Hero />
       <Services />
-      <Contact />
     </Box>
   );
 }
